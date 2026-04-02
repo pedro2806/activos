c79ad1e8590d7b80c2b5b58956c1ae75d6bdc99c
pedro2806 <isc.pedro.mtz@gmail.com>
2026-04-02 11:36:35 -0600

    Se agregan marca, modelo y no serie para select de activos
    Se agregan marca, modelo y no serie para select de activos

--- rentaPrestamo.php
+++ rentaPrestamo.php
@@ -221,7 +221,7 @@
                         var $activoSelect = $('#activoSelect');
                         $activoSelect.empty().append('<option value="">Selecciona...</option>');
                         activos.forEach(function(activo) {
-                            $activoSelect.append('<option value="' + activo.id + '">' + activo.descripcion + '</option>');
+                            $activoSelect.append('<option value="' + activo.id + '">' + activo.descripcion + ' - ma: ' + activo.marca + '- mo ' + activo.modelo + ' (' + activo.no_serie + ')</option>');
                         });                        
                         $activoSelect.select2({
                             theme: 'bootstrap-5'
