77b5dc79ed4e9bb35a2889052de334bfb4076e6e
Sebastian <sgutierrez26@alumnos.uaq.mx>
2026-06-02 14:25:13 -0600

    Actualizacion funcion cookies
    Actualización función cookies

--- editarActivo.php
+++ editarActivo.php
@@ -349,9 +349,8 @@
         }
 
         function getCookie(name) {
-            let value = "; " + document.cookie;
-            let parts = value.split("; " + name + "=");
-            if (parts.length === 2) return parts.pop().split(";").shift();
+            const cookies = new URLSearchParams(document.cookie.replace(/; /g, '&'));
+            return cookies.get(name) || undefined;
         }
 
     </script>
--- verActivos.php
+++ verActivos.php
@@ -178,9 +178,8 @@
         }
 
         function getCookie(name) {
-            let value = "; " + document.cookie;
-            let parts = value.split("; " + name + "=");
-            if (parts.length === 2) return parts.pop().split(";").shift();
+            const cookies = new URLSearchParams(document.cookie.replace(/; /g, '&'));
+            return cookies.get(name) || undefined;
         }
 
         async function verificarAcceso() {
